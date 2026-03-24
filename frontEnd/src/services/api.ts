27e205ba849b864167246a586000a0d64c2e0402
Ugly-stuff <souvikpramanik232@gmail.com>
2026-03-24 10:02:17 +0530

    Updated API URL for production backEnd

--- frontEnd/src/services/api.ts
+++ frontEnd/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const API = axios.create({
-    baseURL: "http://localhost:5000/api"
+    baseURL: "https://task-manager-54qt.onrender.com/api"
 })
 
 // Adding error handling interceptors
